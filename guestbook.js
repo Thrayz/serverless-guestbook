31eaf9ada7b946352b0fab3b10272fe882503799
Thrayz <88392163+Thrayz@users.noreply.github.com>
2021-10-16 15:24:55 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2a5c11bc.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
